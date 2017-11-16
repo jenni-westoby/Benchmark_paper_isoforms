78fef7d86e7cadc2ec5e03e8725ce32e791883f7
jenni-westoby <jenni-westoby@users.noreply.github.com>
2017-11-16 12:03:58 +0000

    Update quantify_real_data.sh

--- scripts/quantify_real_data.sh
+++ scripts/quantify_real_data.sh
@@ -5,8 +5,8 @@ Kallisto() {
       #make a directory for the results of Kallisto for each cell
       filename=$1
       mkdir ../quant_results/$filename
-
-      $TEAM/BLUEPRINT_latest_versions/Benchmarking_pipeline/Simulation/kallisto_linux-v0.43.1/kallisto quant -i $TEAM/BLUEPRINT_latest_versions/Benchmarking_pipeline/Simulation/indices/Kallisto/transcripts.idx --threads=8 --output-dir=../quant_results/$filename ../fastqs/$filename'_1.fastq' ../fastqs/$filename'_2.fastq'
+      #EDIT LINE BELOW WHERE MARKED
+      $TEAM/BLUEPRINT_latest_versions/Benchmarking_pipeline/Simulation/kallisto_linux-v0.43.1/kallisto quant -i <INSERT PATH TO KALLISTO INDEX> --threads=8 --output-dir=../quant_results/$filename ../fastqs/$filename'_1.fastq' ../fastqs/$filename'_2.fastq'
 
 }
 
